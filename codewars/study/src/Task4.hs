9cd5f8496e5fd8ca07503888a07e18b0b6b17e8c
Vasili Litvin <vasili.litvin@stackpath.com>
2020-10-03 15:27:23 +0300

    Tasks1-4

--- codewars/study/src/Task4.hs
+++ codewars/study/src/Task4.hs
@@ -12,4 +12,4 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 -}
 
 countPositivesSumNegatives :: Maybe [Int] -> [Int]
--- countPositivesSumNegatives = Just
+countPositivesSumNegatives = undefined
